b2712f9308f25912bd595dde776ac1cba9416cf5
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-08-14 15:35:55 +0200

    Added ; so code can run

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
 
 // Challenge 1 - Traffic light system
 
-$trafficlight = "red" 
+$trafficlight = "red";
 
 switch ($trafficlight) {
     case 'red':
@@ -18,7 +18,7 @@ switch ($trafficlight) {
 
 // Challenge 2 - Amusement park
 
-$age = "22"
+$age = "22";
 
 If ($age) {
     case 'red':
@@ -34,4 +34,25 @@ If ($age) {
 
 // Challenge 3 - Personalised greeting
 
+$name = "Palesa";
+
+switch ($name) {
+    case 'Lelo':
+        echo "Hey Lelo, it's been a while since we last spoke."
+        break;
+    case 'Mbali'
+        echo "Good day Mbali, so glad to see you again!"
+        break;
+    case 'Palesa'
+        echo "Palesa, your affirmation for the day: Be kind to yourself."
+        break;
+    case 'Nini'
+        echo "Hello Nini, let's get to work."
+        break;
+    case 'Paballo'
+        echo "Paballo you have 11 days to complete the form."
+        break;
+    default:
+        echo "Unknown user"
+}
 ?>
