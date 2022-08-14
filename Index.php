f18d95a37f6bda97b49ebc9d619ebcfdfe0a262e
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-08-14 15:16:19 +0200

    Challenge one traffic light system

---
+++ Index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Challenge 1 - Traffic light system
+
+$trafficlight = "red" 
+
+switch ($trafficlight) {
+    case 'red':
+        echo "stop"
+        break;
+    case 'yellow'
+        echo "slow down"
+        break;
+    case 'green'
+        echo "go"
+        break;
+}
+
+// Challenge 2 - Amusement park
+
+$age = "22"
+
+If ($age) {
+    case 'red':
+        echo "stop"
+        break;
+    case 'yellow'
+        echo "slow down"
+        break;
+    case 'green'
+        echo "go"
+        break;
+}
+
+// Challenge 3 - Personalised greeting
+
+?>
