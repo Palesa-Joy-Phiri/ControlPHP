a5c332ae678468f2030cea154d2f349c4052fdb4
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-08-16 11:32:05 +0200

    Correction of broken code

--- Index.php
+++ Index.php
@@ -2,57 +2,74 @@
 
 // Challenge 1 - Traffic light system
 
-$trafficlight = "red";
+    echo "Challenge 1 - Traffic light system". "<br>";
+
+$trafficlight = "green";
 
 switch ($trafficlight) {
     case 'red':
-        echo "stop"
+        echo "stop";
         break;
-    case 'yellow'
-        echo "slow down"
+    case 'yellow':
+        echo "slow down";
         break;
-    case 'green'
-        echo "go"
+    case 'green':
+        echo "go";
         break;
 }
+
+echo "<br>";
+echo "<br>";
 
 // Challenge 2 - Amusement park
 
-$age = "22";
+    echo "Challenge 2 - Amusement park". "<br>";
 
-If ($age) {
-    case 'red':
-        echo "stop"
-        break;
-    case 'yellow'
-        echo "slow down"
-        break;
-    case 'green'
-        echo "go"
-        break;
+$age = "35";
+
+if($age <2) {
+        echo "Free";
+    } elseif ($age>=2 && $age<=12) {
+        echo "Free";
+    } elseif ($age>=2 && $age<=12) {
+        echo "Pay R20";
+    } elseif ($age>=13 && $age<=17) {
+        echo "Pay R30";
+    } elseif ($age>=18 && $age<=25) {
+        echo "Pay R40";
+    } elseif ($age>=26 && $age<=64) {
+        echo "Pay R60";
+    } elseif ($age>=65) {
+        echo "Pay R30";    
 }
 
+echo "<br>";
+echo "<br>";
+
 // Challenge 3 - Personalised greeting
+
+    echo "Challenge 3 - Personalised greeting". "<br>";
 
 $name = "Palesa";
 
 switch ($name) {
     case 'Lelo':
-        echo "Hey Lelo, it's been a while since we last spoke."
+        echo "Hey Lelo, it's been a while since we last spoke.";
         break;
-    case 'Mbali'
-        echo "Good day Mbali, so glad to see you again!"
+    case 'Mbali':
+        echo "Good day Mbali, so glad to see you again!";
         break;
-    case 'Palesa'
-        echo "Palesa, your affirmation for the day: Be kind to yourself."
+    case 'Palesa':
+        echo "Palesa, your affirmation for the day: Be kind to yourself.";
         break;
-    case 'Nini'
-        echo "Hello Nini, let's get to work."
+    case 'Nini':
+        echo "Hello Nini, let's get to work.";
         break;
-    case 'Paballo'
-        echo "Paballo you have 11 days to complete the form."
+    case 'Paballo':
+        echo "Paballo you have 11 days to complete the form.";
         break;
     default:
-        echo "Unknown user"
+        echo "Unknown user";
+        break;
 }
 ?>
